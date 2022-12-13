9330d62cf1c3764c082dec3340f1e832232ed0d1
Admin <ilinskiy_kirill@mail.ru>
2022-12-13 17:44:36 +0300

    hw8.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Passport irina = new Passport(
+                Collections.singleton("523HQG"),
+                "Ягодкова",
+                "Вишня",
+                "Константиновна", "01.01.1980");
+        Passport sergei = new Passport(
+                Collections.singleton("184AEW"),
+                "Михайлов",
+                "Потап",
+                "Таптыгович",
+                "02.02.1963");
+        Passport andrei = new Passport(
+                Collections.singleton("591KРB"),
+                "Мамбаева",
+                "Любовь",
+                "Сергеевна",
+                "03.03.2000");
+
+        Set<Passport> passportSets = new HashSet<>();
+        passportSets.add(irina);
+        passportSets.add(sergei);
+        passportSets.add(andrei);
+
+        Iterator itr = passportSets.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        System.out.println("Паспорт: " + find(passportSets, "591KРB"));
+
+
     }
+
+    public static Passport find(Set<Passport> passportSets, String number) {
+        for (Passport passports: passportSets) {
+            if (passports.isSetNumber(number)) {
+                return passports;
+            }
+        }
+        return null;
+    }
+
+
+
+
 }
